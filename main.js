09d6fb70ad9793b4530e455ed7582b2c5c75166a
unknown <knosmos@gmail.com>
2024-08-02 01:48:48 +0800

    initial commit, very jank

---
+++ main.js
@@ -0,0 +1,87 @@
+API = "https://rex.mit.edu/api.json";
+fetch(API).then(response => response.json()).then(data => {
+    console.log(data);
+});
+
+function makeEvent(event, config) {
+    let name = event.name;
+    let start_date = new Date(event.start);
+    let end_date = new Date(event.end);
+    
+    let rex_start = new Date(config.start) - 1000 * 60 * 60 * 12;
+    let rex_end = new Date(config.end);
+    let start_pos = (start_date - rex_start) / (rex_end - rex_start) * 10000;
+    let width = (end_date - start_date) / (rex_end - rex_start) * 10000;
+    return {
+        name: name,
+        start: start_date,
+        end: end_date,
+        start_pos: start_pos,
+        width: width,
+        dorm: event.dorm
+    };
+}
+
+function makeSchedule(events, config) {
+    let tracks = []; // non overlapping events
+    let event_objects = [];
+    for (let e of events) {
+        event_objects.push(makeEvent(e, config));
+    }
+    event_objects.sort((a, b) => a.start - b.start);
+    for (let event of event_objects) {
+        let added = false;
+        for (let track of tracks) {
+            if (event.start >= track[track.length - 1].end) {
+                track.push(event);
+                added = true;
+                break;
+            }
+        }
+        if (!added) {
+            tracks.push([event]);
+        }
+    }
+    return tracks;
+}
+
+Vue.component('events-track', {
+    props: ['events', 'config'],
+    template: `
+    <div class="event-grid">
+        <div v-for="event in events">
+            <div class="event" v-bind:style="'left:'+event.start_pos+'px;width:'+event.width+'px'">
+                <div class="card" v-bind:style="'background-color:'+config['colors']['dorms'][event.dorm[0]]">
+                    <b><p>{{ event.name }}</p></b>
+                    <p>{{ event.start.toLocaleTimeString() }}-{{ event.end.toLocaleTimeString() }}</p>
+                </div>
+            </div>
+        </div>
+    </div>`
+});
+
+Vue.component('events-schedule', {
+    props: ['tracks', 'config'],
+    template: `
+    <div class="events-schedule">
+        <div v-for="track in tracks">
+            <events-track :events="track" :config="config"></events-track>
+        </div>
+    </div>`
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        tracks: [],
+        config: {}
+    },
+    mounted() {
+        fetch(API).then(response => response.json()).then(data => {
+            this.config = data;
+            this.tracks = makeSchedule(data.events, data);
+            console.log(this.tracks);
+            console.log(this.config);
+        });
+    },
+});
