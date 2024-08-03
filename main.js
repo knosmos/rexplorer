2d5130fdf03f46dd3850d2a5da574f44cdfa03e0
unknown <knosmos@gmail.com>
2024-08-04 01:07:32 +0800

    change API url

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // UTILITY
 API = "https://rex.mit.edu/api.json";
-AI_API = "http://192.168.0.13:5000/";
+AI_API = "https://rexplorer.onrender.com";
 
 let starred = [];
 if ("starred" in localStorage) {
